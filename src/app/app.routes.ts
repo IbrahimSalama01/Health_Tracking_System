b77b43f1831abd5b96035bd82d8bb1d89607e335
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-05-06 10:39:18 +0300

    feat: define application routes for landing page, contact us, privacy policy, and dashboard with nested routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './Features/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component:LandingPageComponent},
+  {path: 'contact-us', loadComponent: () => import('./Features/contact-us/contact-us.component').then(c => c.ContactUsComponent)},
+  {path: 'privacy-policy', loadComponent: () => import('./Features/privacy-policy/privacy-policy.component').then(c => c.PrivacyPolicyComponent)},
+  {path: 'dashboard', loadComponent: () => import('./Features/Dashboard/dashboard/dashboard.component').then(c => c.DashboardComponent),
+    children:[
+      {path:'',loadComponent: () => import('./Features/Dashboard/Patient/statistics/statistics.component').then(c => c.StatisticsComponent)},
+      {path:'checkups',loadComponent: () => import('./Features/Dashboard/Patient/checkups/checkups.component').then(c => c.CheckupsComponent)},
+      {path:'my-doctors',loadComponent: () => import('./Features/Dashboard/Patient/my-doctors/my-doctors.component').then(c => c.MyDoctorsComponent)}
+    ] }
+
+];
