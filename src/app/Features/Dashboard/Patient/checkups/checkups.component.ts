84d004315ad5580a09beec41be7b42717a1a1770
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-05-06 10:36:12 +0300

    feat: add checkups, my-doctors, and statistics components with HTML, CSS, and TypeScript implementation

---
+++ src/app/Features/Dashboard/Patient/checkups/checkups.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkups',
+  imports: [],
+  templateUrl: './checkups.component.html',
+  styleUrl: './checkups.component.css'
+})
+export class CheckupsComponent {
+
+}
---
+++ src/app/Features/Dashboard/Patient/my-doctors/my-doctors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-doctors',
+  imports: [],
+  templateUrl: './my-doctors.component.html',
+  styleUrl: './my-doctors.component.css'
+})
+export class MyDoctorsComponent {
+
+}
