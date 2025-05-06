fc74f565b2aa227d34ca0c315571c22771144f15
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-05-06 10:34:01 +0300

    feat: add HttpClient provider with fetch support to app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,12 @@ import Material from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(withFetch()),
     provideClientHydration(withEventReplay()),
     providePrimeNG({
       theme: {
