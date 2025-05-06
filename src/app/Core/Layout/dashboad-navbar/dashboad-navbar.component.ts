75aad4d13b5652cfb83fb506e9f8b3def6fe5ca6
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-05-06 10:35:32 +0300

    feat: implement dashboard navbar and sidebar components with styling and user interaction

---
+++ src/app/Core/Layout/dashboad-navbar/dashboad-navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, input } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import {AvatarModule} from 'primeng/avatar';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { user } from '../../../utils/Interfaces';
+
+@Component({
+  selector: 'app-dashboad-navbar',
+  imports: [ToolbarModule,ButtonModule, AvatarModule,IconFieldModule,InputIconModule],
+  templateUrl: './dashboad-navbar.component.html',
+  styleUrl: './dashboad-navbar.component.css'
+})
+export class DashboadNavbarComponent {
+  user = input.required<user>()
+}
--- src/app/Core/Layout/sidebar/sidebar.component.ts
+++ src/app/Core/Layout/sidebar/sidebar.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, input } from '@angular/core';
+import { sidenavItem } from '../../../utils/Interfaces';
+import { SideBarItemComponent } from '../../../Shared/side-bar-item/side-bar-item.component';
+import {RippleModule} from 'primeng/ripple';
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [SideBarItemComponent,RippleModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-
+  sidenavItems = input.required<sidenavItem[]>()
+  hover = false
 }
