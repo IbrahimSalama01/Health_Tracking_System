600cb301bfe7b04b406da0ccc715cbb233b62d21
rafatgomaa <rafatgomaa815@gmail.com>
2025-05-08 15:26:17 +0300

    changes to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { LandingPageComponent } from './Features/landing-page/landing-page.compo
 
 export const routes: Routes = [
   {path: '', component:LandingPageComponent},
+  {path: 'signup', loadComponent: () => import('./Core/Auth/signup/signup.component').then(c => c.SignupComponent)},
+  {path: 'login', loadComponent: () => import('./Core/Auth/login/login.component').then(c => c.LoginComponent)},
+  {path: 'forgot-password', loadComponent: () => import('./Core/Auth/forgot-password/forgot-password.component').then(c => c.ForgotPasswordComponent)},
   {path: 'contact-us', loadComponent: () => import('./Features/contact-us/contact-us.component').then(c => c.ContactUsComponent)},
   {path: 'privacy-policy', loadComponent: () => import('./Features/privacy-policy/privacy-policy.component').then(c => c.PrivacyPolicyComponent)},
   {path: 'dashboard', loadComponent: () => import('./Features/Dashboard/dashboard/dashboard.component').then(c => c.DashboardComponent),
