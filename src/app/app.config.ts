40bccd9d74cbaebdb3833c04a10154d70f84f19d
ahmedaljendy <elgendya166@gmail.com>
2025-05-08 11:27:17 +0300

    feat: update dependencies and enhance routing structure for doctor management features

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,34 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { providePrimeNG } from 'primeng/config';
 import Material from '@primeng/themes/aura';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withFetch()),
     provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-          preset:Material
-      }
-  })
-  ]
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: false,
+        },
+      },
+    }),
+  ],
 };
