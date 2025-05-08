548db2b74742be45099f41027930fa0361b3d82a
ahmedaljendy <elgendya166@gmail.com>
2025-05-08 11:26:59 +0300

    feat: implement MyDoctorsComponent with tabs for accessed, previous, and request doctors

--- src/app/Features/Dashboard/Patient/my-doctors/my-doctors.component.ts
+++ src/app/Features/Dashboard/Patient/my-doctors/my-doctors.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { PrimeTemplate } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { PrimeNG } from 'primeng/config';
+import { Certification } from './../../../../utils/Interfaces';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { TabsModule } from 'primeng/tabs';
 
 @Component({
   selector: 'app-my-doctors',
-  imports: [],
+  imports: [
+    ButtonModule,
+    CardModule,
+    RouterOutlet,
+    TabsModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './my-doctors.component.html',
-  styleUrl: './my-doctors.component.css'
+  styleUrl: './my-doctors.component.css',
 })
 export class MyDoctorsComponent {
-
+  // constructor(private primeng: PrimeNG) {}
+  tabs = [
+    {
+      label: 'Accessed-Doctors',
+      route: 'accessed-doctors',
+      icon: 'pi pi-home',
+    },
+    { label: 'Prev-Doctors', route: 'prev-doctors', icon: 'pi pi-chart-line' },
+    { label: 'Requests', route: 'requests', icon: 'pi pi-cog' },
+  ];
 }
