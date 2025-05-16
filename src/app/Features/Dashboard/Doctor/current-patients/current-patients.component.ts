1068429d477b2224718340550f18460b87007abd
ahmedaljendy <elgendya166@gmail.com>
2025-05-17 00:33:14 +0300

    fix: correct formatting and style issues in login, current patients, and previous doctors components

--- src/app/Features/Dashboard/Doctor/current-patients/current-patients.component.ts
+++ src/app/Features/Dashboard/Doctor/current-patients/current-patients.component.ts
@@ -1,31 +1,31 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { PatientService } from '../../../../Core/services/auth/patient.service';
+import { PatientService } from '../../../../Core/Services/auth/patient.service';
 import { PatientCardComponent } from './patient-card/patient-card/patient-card.component';
 
 @Component({
   selector: 'app-current-patients',
   imports: [PatientCardComponent],
   templateUrl: './current-patients.component.html',
-  styleUrl: './current-patients.component.css'
+  styleUrl: './current-patients.component.css',
 })
 export class CurrentPatientsComponent implements OnInit {
-  patients = <any> []
-  private patientService = inject(PatientService)
-  ngOnInit(){
+  patients = <any>[];
+  private patientService = inject(PatientService);
+  ngOnInit() {
     this.getDoctorPatients();
   }
   getDoctorPatients() {
     this.patientService.getPatients().subscribe({
-      next: (res) => {
+      next: res => {
         this.patients = res.patients;
         console.log(this.patients);
       },
-      error: (err) => {
+      error: err => {
         console.log(err);
       },
       complete: () => {
         console.log('done');
-      }
+      },
     });
-  } 
+  }
 }
